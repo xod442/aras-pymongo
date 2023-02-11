642456b8bea3230fbb81ecb979817085da9c8376
Rick Kauffman <rick@rickkauffman.com>
2023-02-10 17:08:56 -0700

    t

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 '''
- _______  ___   __   __  _______  ___      _______    _______  _______  ___
-|       ||   | |  |_|  ||       ||   |    |       |  |   _   ||       ||   |
-|  _____||   | |       ||    _  ||   |    |    ___|  |  |_|  ||    _  ||   |
-| |_____ |   | |       ||   |_| ||   |    |   |___   |       ||   |_| ||   |
-|_____  ||   | |       ||    ___||   |___ |    ___|  |       ||    ___||   |
- _____| ||   | | ||_|| ||   |    |       ||   |___   |   _   ||   |    |   |
-|_______||___| |_|   |_||___|    |_______||_______|  |__| |__||___|    |___|
 
+    _   ___    _   ___
+   /_\ | _ \  /_\ / __|___ _ __ _  _ _ __  ___ _ _  __ _ ___
+  / _ \|   / / _ \\__ \___| '_ \ || | '  \/ _ \ ' \/ _` / _ \
+ /_/ \_\_|_\/_/ \_\___/   | .__/\_, |_|_|_\___/_||_\__, \___/
+                          |_|   |__/               |___/
+
+ARAS PYmongo example
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0.
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+__author__ = "@netwookie"
+__credits__ = ["Rick Kauffman"]
+__license__ = "Apache2"
+__version__ = "0.1.1"
+__maintainer__ = "Rick Kauffman"
+__status__ = "Alpha"
+
+'''
 
 ARAS PYmongo example
 
